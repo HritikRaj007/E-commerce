66f08b26eb914fbee004e2553da52eab6c9020bf
Hritik Raj <rajhritik2000@gmail.com>
2021-06-14 13:00:49 +0530

    initial data inserted in database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+
 dotenv.config()
 
 connectDB()
@@ -13,14 +14,7 @@ app.get('/',(req,res) => {
     res.send("Please wait we server starting in a second")
 })
 
-app.get('/api/products',(req,res) =>{
-    res.send(products)
-})
-
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find((p)=> p._id===req.params.id)
-    res.send(product)
-})
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
