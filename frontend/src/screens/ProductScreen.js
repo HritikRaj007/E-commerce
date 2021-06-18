ccde1a3063b88c7d8fd38bc13fddd6c5b1cf532d
Hritik Raj <rajhritik2000@gmail.com>
2021-06-18 15:18:28 +0530

    add to cart functionality added

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,18 +9,19 @@ import { listProductDetails } from '../actions/productActions'
 
 
 const ProductScreen = ({ history,match }) => {
-    const [qty, setQty] = useState(0)
+    const [qty, setQty] = useState(1)
     const dispatch = useDispatch()
     const productDetails = useSelector((state) => state.productDetails)
     const { loading, error, product } = productDetails
-    const addToCartHandler = () => {
-      history.push(`/cart/${match.params.id}?qty=${qty}`)
-    }
+    
 
     useEffect(() => {
       dispatch(listProductDetails(match.params.id))
     }, [dispatch, match])
-
+    
+    const addToCartHandler = () => {
+      history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
 
     return (
       <>
