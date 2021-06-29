50b1c4d73511a8362279b1bef94cf983b70b5012
Hritik Raj <rajhritik2000@gmail.com>
2021-06-29 14:56:48 +0530

    fixed some errors

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -14,12 +14,18 @@ const CartScreen = ({ match, location, history }) => {
   
     const cart = useSelector((state) => state.cart)
     const { cartItems } = cart
+
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
   
     useEffect(() => {
+      if(!userInfo){
+        history.push('/login')
+      }
       if (productId) {
         dispatch(addToCart(productId, qty))
       }
-    }, [dispatch, productId, qty])
+    }, [dispatch, productId, qty,userInfo,history])
   
     const removeFromCartHandler = (id) => {
       dispatch(removeFromCart(id))
